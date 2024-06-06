332169fc2666b05168e977958cd364b23dd18d3c
Aldric Bocquet <aldricbocquet@gmail.com>
2024-06-06 04:18:53 +0200

    ready to publish ?

--- addons/godot-dataview/godot-dataview.gd
+++ addons/godot-dataview/godot-dataview.gd
@@ -2,13 +2,8 @@
 extends EditorPlugin
 
 
-func loadSvgIcon(path, size):
-	var img = Image.new()
-	img.load_svg_from_buffer(FileAccess.get_file_as_bytes(path), float(size) / load(path).get_size().x)
-	return ImageTexture.create_from_image(img)
-
 func _enter_tree():
-	add_custom_type("DataView", "Control", preload("dataview.gd"), loadSvgIcon("res://icon.svg", 16))
+	add_custom_type("DataView", "Control", preload("dataview.gd"), preload("icon-16x16.png"))
 	pass
 
 
